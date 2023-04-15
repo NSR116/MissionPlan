2227d361632b4d347ed833bcf89b6a499faf8268
NSR116m <nourasaadr1@gmail.com>
2023-04-16 01:58:32 +0300

    Updates

--- Assets/Scripts/ObjectSetting.cs
+++ Assets/Scripts/ObjectSetting.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class ObjectSetting : MonoBehaviour
 {
     [SerializeField] LayerMask layer;
+    [SerializeField] GameObject panel;
     private bool isPlaced = false;
+    private bool isRotated = false;
     private Vector2 turn;
 
     // Update is called once per frame
@@ -36,31 +40,57 @@ public class ObjectSetting : MonoBehaviour
 
     void rotateObject()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Input.GetMouseButton(0))
+        if (!isRotated)
         {
-            if(Physics.Raycast(ray.origin, ray.direction, out RaycastHit hitInfo, Mathf.Infinity))
-            {
-                //print(hitInfo.transform.gameObject.name);
+            turn.x += Input.GetAxis("Mouse X");
+            turn.y += Input.GetAxis("Mouse Y");
+            //Vector2 turnSpeed = new Vector2(turn.x, turn.y) * Time.deltaTime * 50f;
+            transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isRotated = true;
+        }     
+        
+    }
+
+    void OnMouseOver()
+    {
+        if(isPlaced && isRotated)
+        {
+            print("!!!");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hitInfo, Mathf.Infinity))
+            {
                 if (hitInfo.transform.gameObject.name == "Soldier(Clone)" ||
                     hitInfo.transform.gameObject.name == "Medical(Clone)" ||
                     hitInfo.transform.gameObject.name == "Tank(Clone)" ||
                     hitInfo.transform.gameObject.name == "Transportation(Clone)")
                 {
-                    //print(hitInfo.transform.gameObject.name);
-
-                    turn.x += Input.GetAxis("Mouse X");
-                    turn.y += Input.GetAxis("Mouse Y");
-                    hitInfo.transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+                    print(hitInfo.transform.gameObject.name);
+                    panel.SetActive(true);
                 }
             }
         }
+    }
 
-        else
+    void OnMouseExit()
+    {
+        if(isPlaced && isRotated)
         {
-            return;
+            panel.SetActive(false);
         }
+    }
+
+    public void delete()
+    {
+        Destroy(gameObject);
+    }
+
+    public void edit()
+    {
+        isPlaced = false;
+        isRotated = false;
     }
 }
